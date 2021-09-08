434f4d027b14a7aac128ced371161ca3aac101ee
Connor Kolb <71943701+connor-kolb-13@users.noreply.github.com>
2021-09-08 13:54:30 -0500

    Update DataProcessor.java

--- DataProcessor.java
+++ DataProcessor.java
@@ -1,11 +1,5 @@
 // --== CS400 File Header Information ==--
 // Name: Connor Kolb
-// Email: cwkolb@wisc.edu
-// Team: KA
-// Role: Data Wrangler 1
-// TA: Sid
-// Lecturer: Gary Dahl
-// Notes to Grader: n/a
 
 import java.io.File;
 import java.io.FileNotFoundException;
